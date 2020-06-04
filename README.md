38cec8007c3a2131fbd6b47eef48be61e98a24db
Vikrant github-projets <vikrantmails@aol.com>
2020-06-04 16:18:08 -0400

    06-04-20:03 01_Functions in Python

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Python Based Fun Game.
 Core aspects of the Python language are covered in this game
 
 # Introduction 
-1.Print, Input, Variables, and if/else Statements
+1.Print, Input, Variables, and if/else Statements <br />
+2.Functions <br />
--- python_pirate_trader.py
+++ python_pirate_trader.py
@@ -1,23 +1,38 @@
 import os
-print("Welcome to Python Pirate Trader")
+MENU_DIVIDER = "------------------------------"
+GAME_TITLE = "Python Pirate Trader 0.1A"
 
-firm_name = input("Please enter your Firm Name: ")
+def welcome_message():
+    print("Welcome to Python Pirate Trader")
 
-print("Firm Name:  "+ firm_name)
+def get_firm_name():
+    firm_name = input("Please enter your Firm Name: ")
+    return firm_name
 
-starting_options = input("How do you wish to start. 1) Cash & Debt 2) Cannons no debt.: ")
+def get_starting_options():
+    starting_options = input("How do you wish to start. 1) Cash & Debt 2) Cannons No debt. : ")
+    if starting_options == "1":
+        cash = 250
+        debt = cash
+        cannons = 0
+    else:
+        cash = 0
+        debt = 0
+        cannons = 5
+    return cash, debt,cannons
+   
+# Start Game
+welcome_message()
+firm_name = get_firm_name()
+cash,debt,cannons = get_starting_options()
 
-if starting_options == "1":
-    cash = 250
-    debt = cash
-    cannons = 0
-else:
-    cash = 0
-    debt = 0
-    cannons = 5
-
+# Display Main Game Interface
 os.system("cls")
-print("------------------------------")
-print("Python Pirate Trader 0.1A")
-print("------------------------------")
-# print("Cash = " + cash)
+print(MENU_DIVIDER)
+print(GAME_TITLE)
+print(MENU_DIVIDER)
+
+print("Firm Name: " + firm_name)
+print("Cash: " + str(cash))
+print("Debt: " + str(debt))
+print("Cannons: " + str(cannons))
